5d8460b10a6a31a55fc8bc7c8762bcd1d2e6d2e6
Praveen Prajapati <pk20210845@gmail.com>
2023-06-25 20:16:21 +0530

    initial setup for frontend and backend

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright &copy; {currentYear}</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
